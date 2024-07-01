8f296d783181c8a02f5ea7f34331be8b013b2f80
Your Name <you@example.com>
2024-07-01 17:29:20 +0900

    Change format to avoid exposing defaults

--- bar.h
+++ bar.h
@@ -3,11 +3,14 @@
 #include <string>
 
 // Customizable values, adjust to liking.
-int DEFAULT_TITLE_LENGTH                = 5;      // Default is 5
-std::string DEFAULT_CONTENT_CHARACTER   = "=";    // Default is "="
-std::string DEFAULT_SPACER_CHARACTER    = " ";    // Default is " "
-std::string DEFAULT_END_CHARACTER       = "\n";   // Default is "\n"
-std::string DEFAULT_EOL_CHARACTER       = "\r";   // Default is "\r"
+namespace BarSettings
+{
+   int DEFAULT_TITLE_LENGTH                = 5;      // Default is 5
+   std::string DEFAULT_CONTENT_CHARACTER   = "=";    // Default is "="
+   std::string DEFAULT_SPACER_CHARACTER    = " ";    // Default is " "
+   std::string DEFAULT_END_CHARACTER       = "\n";   // Default is "\n"
+   std::string DEFAULT_EOL_CHARACTER       = "\r";   // Default is "\r"
+}
 
 struct Bar
 {
@@ -21,10 +24,10 @@ struct Bar
    bool NUMERAL_MODE                    = false;  // Numeral mode is a simplified mode that displays minimal information.
 
    int MAX_TITLE_LENGTH;
-   std::string CONTENT_CHARACTER        = DEFAULT_CONTENT_CHARACTER;
-   std::string SPACER_CHARACTER         = DEFAULT_SPACER_CHARACTER;
-   std::string END_CHARACTER            = DEFAULT_END_CHARACTER;
-   std::string EOL_CHARACTER            = DEFAULT_EOL_CHARACTER;
+   std::string CONTENT_CHARACTER        = BarSettings::DEFAULT_CONTENT_CHARACTER;
+   std::string SPACER_CHARACTER         = BarSettings::DEFAULT_SPACER_CHARACTER;
+   std::string END_CHARACTER            = BarSettings::DEFAULT_END_CHARACTER;
+   std::string EOL_CHARACTER            = BarSettings::DEFAULT_EOL_CHARACTER;
 
    int CUSTOM_TITLE_LENGTH;                     // To nullify, set to 0
    std::string CUSTOM_CONTENT_CHARACTER = "";   // To nullify, set to ""
@@ -33,115 +36,42 @@ struct Bar
    std::string CUSTOM_EOL_CHARACTER     = "";   // To nullify, set to ""
 };
 
-enum BAR_FLAGS
-{
-   BAR_INIT_CORE,   // Initialize only core components
-   BAR_INIT_SAFE,   // Initialize only the minimum components
-   BAR_INIT,        // Initialize the manager
-   BAR_QUIT_CORE,   // Quit only the core components (rare)
-   BAR_QUIT         // Quit bar manager
-};
-
 namespace BarMan
 {
-   int Initialize(Bar* bar, BAR_FLAGS FLAG)
+   enum BAR_FLAGS
    {
-      bar->MAX_TITLE_LENGTH  = (bar->CUSTOM_TITLE_LENGTH == 0)       ? DEFAULT_TITLE_LENGTH      : bar->CUSTOM_TITLE_LENGTH;        // Configure MAX_TITLE_LENGTH
-      bar->CONTENT_CHARACTER = (bar->CUSTOM_CONTENT_CHARACTER == "") ? DEFAULT_CONTENT_CHARACTER : bar->CUSTOM_CONTENT_CHARACTER;   // Configure CONTENT_CHARACTER
-      bar->SPACER_CHARACTER = (bar->CUSTOM_SPACER_CHARACTER == "")  ? DEFAULT_SPACER_CHARACTER  : bar->CUSTOM_SPACER_CHARACTER;    // Configure SPACER_CHARACTER
-      bar->END_CHARACTER     = (bar->CUSTOM_END_CHARACTER == "")     ? DEFAULT_END_CHARACTER     : bar->CUSTOM_END_CHARACTER;       // Configure END_CHARACTER
-      bar->EOL_CHARACTER     = (bar->CUSTOM_EOL_CHARACTER == "")     ? DEFAULT_EOL_CHARACTER     : bar->CUSTOM_EOL_CHARACTER;       // Configure EOL_CHARACTER
+      INIT_CORE,   // Initialize only core components
+      INIT_SAFE,   // Initialize only the minimum components
+      INIT,        // Initialize the manager
+      QUIT_CORE,   // Quit only the core components (rare)
+      QUIT         // Quit bar manager
+   };
+
+   int Init(Bar* bar, BAR_FLAGS FLAG)
+   {
+      bar->MAX_TITLE_LENGTH                = (bar->CUSTOM_TITLE_LENGTH == 0)       ? BarSettings::DEFAULT_TITLE_LENGTH      : bar->CUSTOM_TITLE_LENGTH;        // Configure MAX_TITLE_LENGTH
+      bar->CONTENT_CHARACTER               = (bar->CUSTOM_CONTENT_CHARACTER == "") ? BarSettings::DEFAULT_CONTENT_CHARACTER : bar->CUSTOM_CONTENT_CHARACTER;   // Configure CONTENT_CHARACTER
+      bar->SPACER_CHARACTER                = (bar->CUSTOM_SPACER_CHARACTER == "")  ? BarSettings::DEFAULT_SPACER_CHARACTER  : bar->CUSTOM_SPACER_CHARACTER;    // Configure SPACER_CHARACTER
+      bar->END_CHARACTER                   = (bar->CUSTOM_END_CHARACTER == "")     ? BarSettings::DEFAULT_END_CHARACTER     : bar->CUSTOM_END_CHARACTER;       // Configure END_CHARACTER
+      bar->EOL_CHARACTER                   = (bar->CUSTOM_EOL_CHARACTER == "")     ? BarSettings::DEFAULT_EOL_CHARACTER     : bar->CUSTOM_EOL_CHARACTER;       // Configure EOL_CHARACTER
       return 0;
    }
 
    int Quit(Bar* bar, BAR_FLAGS FLAG)
    {
-      bar->CONTENT_CHARACTER = "";
-      bar->SPACER_CHARACTER  = "";
-      bar->END_CHARACTER     = "";
-      bar->EOL_CHARACTER     = "";
-      bar->MAX_TITLE_LENGTH  = 0;
+      bar->CONTENT_CHARACTER               = "";
+      bar->SPACER_CHARACTER                = "";
+      bar->END_CHARACTER                   = "";
+      bar->EOL_CHARACTER                   = "";
+      bar->MAX_TITLE_LENGTH                = 0;
       return 0;
    }
-}
-
-void UpdateBar(Bar* bar)
-{
-   #ifdef NDEBUG // If release do not output logs
-   return;
-   #else
-   if (bar->TRUNCATE_TITLES)
-   {
-      // Strip bar to MAX_TITLE_LENGTH letters
-      if (bar->TITLE.length() > bar->MAX_TITLE_LENGTH)
-      {
-         bar->TITLE = "(...)";
-      }
-
-      if (bar->TITLE.length() < bar->MAX_TITLE_LENGTH)
-      {
-         int o = bar->MAX_TITLE_LENGTH - bar->TITLE.length();
-         int i = 0;
-
-         while (i < o)
-         {
-            bar->TITLE += DEFAULT_SPACER_CHARACTER;
-            i++;
-         }
-      }
-   }
-
-   // Normal bar mode
-   if (!bar->NUMERAL_MODE && bar->SHOW_CONTENT)
-   {
-      // Start Bar
-      if (bar->SHOW_TITLE) std::cout << "[ " << bar->TITLE << " ] [ ";
-      else std::cout << "[ ";
-
-      // Actual contents
-      if (bar->PROGRESS <= 100)
-      {
-         for (int i = 0; i < bar->PROGRESS; i++)
-         {
-               std::cout << bar->CONTENT_CHARACTER;
-         }
-
-         for (int i = 0; i < 100 - bar->PROGRESS; i++)
-         {
-               std::cout << bar->SPACER_CHARACTER;
-         }
-      }
-
-      // End bar, if last out newline.
-      if (bar->APPEND_PERCENTAGE)
-      {
-         if (bar->PROGRESS < 100) std::cout << " ] " << bar->PROGRESS << "%" << bar->EOL_CHARACTER;
-         if (bar->PROGRESS >= 100) std::cout << " ] " << bar->PROGRESS << "%" << bar->END_CHARACTER;
-      }
-      else
-      {
-         if (bar->PROGRESS < 100) std::cout << " ]" << bar->EOL_CHARACTER;
-         if (bar->PROGRESS >= 100) std::cout << " ]" << bar->END_CHARACTER;
-      }
-   }
-
-   if (bar->NUMERAL_MODE)
-   {
-      if (bar->SHOW_TITLE) std::cout << bar->TITLE << ", ";
-      if (bar->APPEND_PERCENTAGE)
-      {
-         if (bar->PROGRESS < 100) std::cout << "Progress: " << bar->PROGRESS << "%" << bar->EOL_CHARACTER;
-         if (bar->PROGRESS >= 100) std::cout << "Progress: " << bar->PROGRESS << "%" << bar->END_CHARACTER;
-      }
-   }
-   return;
-   #endif
 }
 
 // Utilities that are not required to execute, but still are nice to have
 namespace BarUtils 
 {
-   // Fill this out and pass it to the InitializeBar utility to easily create a bar.
+   // Fill this out and pass it to the Create utility to easily create a bar.
    struct BarInitForm
    {
       std::string title;
@@ -154,50 +84,131 @@ namespace BarUtils
       bool NUMERAL_MODE                    = false;   // Numeral mode is a simplified mode that displays minimal information.
 
       int MAX_TITLE_LENGTH;
-      std::string CONTENT_CHARACTER        = DEFAULT_CONTENT_CHARACTER;
-      std::string SPACER_CHARACTER         = DEFAULT_SPACER_CHARACTER;
-      std::string END_CHARACTER            = DEFAULT_END_CHARACTER;
-      std::string EOL_CHARACTER            = DEFAULT_EOL_CHARACTER;
+      std::string CONTENT_CHARACTER        = BarSettings::DEFAULT_CONTENT_CHARACTER;
+      std::string SPACER_CHARACTER         = BarSettings::DEFAULT_SPACER_CHARACTER;
+      std::string END_CHARACTER            = BarSettings::DEFAULT_END_CHARACTER;
+      std::string EOL_CHARACTER            = BarSettings::DEFAULT_EOL_CHARACTER;
 
-      int CUSTOM_TITLE_LENGTH;                     // To nullify, set to 0
+      int CUSTOM_TITLE_LENGTH              = 0;                     // To nullify, set to 0
       std::string CUSTOM_CONTENT_CHARACTER = "";   // To nullify, set to ""
       std::string CUSTOM_SPACER_CHARACTER  = "";   // To nullify, set to ""
       std::string CUSTOM_END_CHARACTER     = "";   // To nullify, set to ""
       std::string CUSTOM_EOL_CHARACTER     = "";   // To nullify, set to ""
    };
 
-   Bar* Initialize(BarInitForm form)
+   Bar* Create(std::string title, int startProgress = 0)
    {
-      Bar* bar                 = new Bar;
-      bar->PROGRESS            = form.startProgress;
-      bar->TITLE               = form.title;
-      bar->TRUNCATE_TITLES     = form.TRUNCATE_TITLES;
-      bar->APPEND_PERCENTAGE   = form.APPEND_PERCENTAGE;
-      bar->SHOW_TITLE          = form.SHOW_TITLE;
-      bar->SHOW_CONTENT        = form.SHOW_CONTENT;
-      bar->MAX_TITLE_LENGTH    = (form.CUSTOM_TITLE_LENGTH == 0)       ? DEFAULT_TITLE_LENGTH      : form.CUSTOM_TITLE_LENGTH;
-      bar->CONTENT_CHARACTER   = (form.CUSTOM_CONTENT_CHARACTER == "") ? DEFAULT_CONTENT_CHARACTER : form.CUSTOM_CONTENT_CHARACTER;
-      bar->SPACER_CHARACTER    = (form.CUSTOM_SPACER_CHARACTER == "")  ? DEFAULT_SPACER_CHARACTER  : form.CUSTOM_SPACER_CHARACTER;
-      bar->END_CHARACTER       = (form.CUSTOM_END_CHARACTER == "")     ? DEFAULT_END_CHARACTER     : form.CUSTOM_END_CHARACTER;
-      bar->EOL_CHARACTER       = (form.CUSTOM_EOL_CHARACTER == "")     ? DEFAULT_EOL_CHARACTER     : form.CUSTOM_EOL_CHARACTER;
+      Bar* bar                             = new Bar;
+      bar->TITLE                           = title;
+      bar->PROGRESS                        = startProgress;
+      return bar;
+   }
+
+   Bar* Create(BarInitForm form)
+   {
+      Bar* bar                             = new Bar;
+      bar->PROGRESS                        = form.startProgress;
+      bar->TITLE                           = form.title;
+      bar->TRUNCATE_TITLES                 = form.TRUNCATE_TITLES;
+      bar->APPEND_PERCENTAGE               = form.APPEND_PERCENTAGE;
+      bar->SHOW_TITLE                      = form.SHOW_TITLE;
+      bar->SHOW_CONTENT                    = form.SHOW_CONTENT;
+      bar->MAX_TITLE_LENGTH                = (form.CUSTOM_TITLE_LENGTH == 0)       ? BarSettings::DEFAULT_TITLE_LENGTH      : form.CUSTOM_TITLE_LENGTH;
+      bar->CONTENT_CHARACTER               = (form.CUSTOM_CONTENT_CHARACTER == "") ? BarSettings::DEFAULT_CONTENT_CHARACTER : form.CUSTOM_CONTENT_CHARACTER;
+      bar->SPACER_CHARACTER                = (form.CUSTOM_SPACER_CHARACTER == "")  ? BarSettings::DEFAULT_SPACER_CHARACTER  : form.CUSTOM_SPACER_CHARACTER;
+      bar->END_CHARACTER                   = (form.CUSTOM_END_CHARACTER == "")     ? BarSettings::DEFAULT_END_CHARACTER     : form.CUSTOM_END_CHARACTER;
+      bar->EOL_CHARACTER                   = (form.CUSTOM_EOL_CHARACTER == "")     ? BarSettings::DEFAULT_EOL_CHARACTER     : form.CUSTOM_EOL_CHARACTER;
       return bar;
    }
 
    // Free bar from memory
-   void DestroyBar(Bar* bar)
+   void Delete(Bar* bar)
    {
       delete(bar);
    }
 
    // Increment the bar's progress by incrementLevel.
-   void IncrementProgress(Bar* bar, int incrementLevel = 1)
+   void Increment(Bar* bar, int incrementLevel = 1)
    {
       bar->PROGRESS += incrementLevel;
    }
 
    // Decrement the bar's progress by decrementLevel.
-   void DecrementProgress(Bar* bar, int decrementLevel = 1)
+   void Decrement(Bar* bar, int decrementLevel = 1)
    {
       bar->PROGRESS -= decrementLevel;
+   }
+
+   void Update(Bar* bar)
+   {
+      #ifdef NDEBUG // If release do not output logs
+      return;
+      #else
+      if (bar->TRUNCATE_TITLES)
+      {
+         // Strip bar to MAX_TITLE_LENGTH letters
+         if (bar->TITLE.length() > bar->MAX_TITLE_LENGTH)
+         {
+            bar->TITLE = "(...)";
+         }
+
+         if (bar->TITLE.length() < bar->MAX_TITLE_LENGTH)
+         {
+            int o = bar->MAX_TITLE_LENGTH - bar->TITLE.length();
+            int i = 0;
+
+            while (i < o)
+            {
+               bar->TITLE += bar->SPACER_CHARACTER;
+               i++;
+            }
+         }
+      }
+
+      // Normal bar mode
+      if (!bar->NUMERAL_MODE && bar->SHOW_CONTENT)
+      {
+         // Start Bar
+         if (bar->SHOW_TITLE) std::cout << "[ " << bar->TITLE << " ] [ ";
+         else std::cout << "[ ";
+
+         // Actual contents
+         if (bar->PROGRESS <= 100)
+         {
+            for (int i = 0; i < bar->PROGRESS; i++)
+            {
+                  std::cout << bar->CONTENT_CHARACTER;
+            }
+
+            for (int i = 0; i < 100 - bar->PROGRESS; i++)
+            {
+                  std::cout << bar->SPACER_CHARACTER;
+            }
+         }
+
+         // End bar, if last out newline.
+         if (bar->APPEND_PERCENTAGE)
+         {
+            if (bar->PROGRESS < 100) std::cout  << " ] " << bar->PROGRESS << "%" << bar->EOL_CHARACTER;
+            if (bar->PROGRESS >= 100) std::cout << " ] " << bar->PROGRESS << "%" << bar->END_CHARACTER;
+         }
+         else
+         {
+            if (bar->PROGRESS < 100) std::cout  << " ]" << bar->EOL_CHARACTER;
+            if (bar->PROGRESS >= 100) std::cout << " ]" << bar->END_CHARACTER;
+         }
+      }
+
+      if (bar->NUMERAL_MODE)
+      {
+         if (bar->SHOW_TITLE) std::cout << bar->TITLE << ", ";
+         if (bar->APPEND_PERCENTAGE)
+         {
+            if (bar->PROGRESS < 100) std::cout  << "Progress: " << bar->PROGRESS << "%" << bar->EOL_CHARACTER;
+            if (bar->PROGRESS >= 100) std::cout << "Progress: " << bar->PROGRESS << "%" << bar->END_CHARACTER;
+         }
+      }
+      return;
+      #endif
    }
 }
